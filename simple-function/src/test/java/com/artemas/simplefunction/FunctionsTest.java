0b1f3fef29bfdf1600a38eb8d1dfd01ac83e7f75
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-12-01 00:06:31 +0000

    Add README

--- simple-function/src/test/java/com/artemas/simplefunction/FunctionsTest.java
+++ simple-function/src/test/java/com/artemas/simplefunction/FunctionsTest.java
@@ -24,4 +24,12 @@ class FunctionsTest {
 
         assertThat(result).isEqualTo("sametra");
     }
+
+    @Test
+    @DisplayName("Should return a string uppercased and then reversed given a normal string")
+    void toUpperCaseAndThenReversed() {
+        final String result = functions.uppercase().andThen(functions.reverse()).apply("takudzwa");
+
+        assertThat(result).isEqualTo("AWZDUKAT");
+    }
 }

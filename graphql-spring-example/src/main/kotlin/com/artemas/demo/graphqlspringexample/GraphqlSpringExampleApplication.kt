74ef1b1534ebe7382915ab9b653934ff34ed6d7c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-12-25 01:44:37 +0000

    first commit

---
+++ graphql-spring-example/src/main/kotlin/com/artemas/demo/graphqlspringexample/GraphqlSpringExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.artemas.demo.graphqlspringexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlSpringExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlSpringExampleApplication>(*args)
+}
---
+++ graphql-spring-example/src/test/kotlin/com/artemas/demo/graphqlspringexample/GraphqlSpringExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artemas.demo.graphqlspringexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlSpringExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
